f6f492397c205ef34b27b34e421ba541327b304b
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-02-22 16:23:43 +0530

    added configuration for browserlist

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
 const heading = React.createElement("h1", {id: "heading"}, "hello world from react!");
 
 const parent = React.createElement("div",{id: "parent"},
-React.createElement("div",{id:"child"},React.createElement("h1",{},"h1 Tag")));
+React.createElement("div",{id:"child"},React.createElement("h1",{},"h1 is created")));
 console.log(heading) // it is not h1 it is object.
 
 console.log(parent)
