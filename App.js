8f2e53183486039c535ea420233a753583538663
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-02-23 05:57:26 +0530

    Practiced the React component

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-const heading = React.createElement("h1", {id: "heading"}, "hello world from react!");
+// React.createElement =>  kReactElement(Object) => render through Dom (html Element):
+const heading = React.createElement("h1", {id:"heading"}, "Namaste React");
 
-const parent = React.createElement("div",{id: "parent"},
-React.createElement("div",{id:"child"},React.createElement("h1",{},"h1 is created")));
-console.log(heading) // it is not h1 it is object.
+//JSX is  HTML-like syntax but JSX is not a HTML in JS.
+//JSX (Transpiled before it reaches the JS) -bundler -babel
 
-console.log(parent)
+//JSX => babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+const jsxHeading = <h1>Namaste React using JSX</h1>;
+
+//React elements
+const head = <span>This Is React Element </span>;
+const idea = (
+
+<h1>{head}/this idea is about a boy who last himself in search for his mission</h1>)
+
+
+                    // always use Upper case when creating react component 
+// React component 
+const Title = () => <h1>This Is React Tutorial</h1>
+
+function Title1(){
+    return <h1>This Is React Tutorial 1.0</h1>
+};
+ // React component
+  // Accessing component in nested is also called  component composition
+const HeadingComponent = () => (
+    <div>
+    {Title()}
+    <Title1/>
+    <h1>React is amazing I love React</h1>
+    {idea}
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); // this render method convert object and convert in h1 
+
+root.render(<HeadingComponent/>);
